78ff3c464aefda31dab1deee16f56e4cb28fefa1
Blackstar Network Ltd <140379610+blackstarnet@users.noreply.github.com>
2023-12-07 15:13:12 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Function to display a message in the console
+function displayMessage() {
+  console.log("Hello, world!");
+}
+
+// Function to calculate the sum of two numbers
+function calculateSum(a, b) {
+  return a + b;
+}
